a3ac75d47f204e89e3d03afb6a80e88e99e2b846
Nelson Menezes <nemzes@gmail.com>
2022-05-13 11:25:49 +0200

    Add CORS support for development

--- backend/src/Altinn.Authorizationadmin/Altinn.Authorizationadmin/Program.cs
+++ backend/src/Altinn.Authorizationadmin/Altinn.Authorizationadmin/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000");
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();

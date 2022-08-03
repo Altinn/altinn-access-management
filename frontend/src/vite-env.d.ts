8734cdf68d90d340ec761310df260f0dfa72b2b4
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-08-03 14:06:06 +0200

    add svg support

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}

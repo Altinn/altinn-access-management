50e7e59e4a61113e58a52a12dbc77e6304e71ea1
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-09-02 10:33:48 +0200

    Add dropdown

--- frontend/src/components/ResourceRegistry/ResourceRegistry.tsx
+++ frontend/src/components/ResourceRegistry/ResourceRegistry.tsx
@@ -2,32 +2,53 @@ import { FC, useState } from 'react';
 import classes from './ResourceRegistry.module.css';
 import { TextField, Button } from '@altinn/altinn-design-system';
 import { PopoverPanel } from '../Common/General/Panel';
-import * as RadixPopover from '@radix-ui/react-popover';
 import { ReactComponent as HelpIcon } from './HelpIcon.svg';
-import SelectSearch from 'react-select-search';
+import Select from 'react-select';
 
-export interface ResourceRegistryProps {
-  id: string;
-  title: string;
-  organisationNumber: string;
-  orgCode: string;
-  telephone: string;
-  email: string;
-  isPartOf: string;
-  hasPart: string;
-  homepage: string;
-  type: string[];
-  sector: string[];
-}
+const ResourceRegistry: FC = () => {
+  const [isValidIdentifier, setIsValidIdentifier] = useState(true);
+  const [isValidTitle, setIsValidTitle] = useState(true);
+  const [isValidDescription, setIsValidDescription] = useState(true);
+  const [isValidOrganizationNumber, setIsValidOrganizationNumber] =
+    useState(true);
+  const [isValidOrgCode, setIsValidOrgCode] = useState(true);
+  const [isValidPhone, setIsValidPhone] = useState(true);
+  const [isValidPartOf, setIsValidPartOf] = useState(true);
+  const [isValidHomepage, setIsValidHomepage] = useState(true);
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [selectTypes, setSelectTypes] = useState([]);
+  const [selectSectors, setSelectSectors] = useState([]);
+  const [id, setId] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [organizationNumber, setOrganizationNumber] = useState('');
+  const [orgCode, setOrgcode] = useState('');
+  const [phone, setPhone] = useState('');
+  const [partOf, setPartOf] = useState('');
+  const [homepage, setHomepage] = useState('');
+  const [email, setEmail] = useState('');
 
-const ResourceRegistry: FC<ResourceRegistryProps> = () => {
-  const idOptions = ['Id 1', 'Id 2', 'Id 3'];
-  const titles = [
-    { name: 'Title 1', value: 'title1' },
-    { name: 'Title 2', value: 'title2' },
-    { name: 'Title 3', value: 'title3' },
+  const types = [
+    { value: 'type1', label: 'Type 1' },
+    { value: 'type2', label: 'Type 2' },
+    { value: 'type3', label: 'Type 3' },
+    { value: 'type4', label: 'Type 4' },
+    { value: 'type5', label: 'Type 5' },
+    { value: 'type6', label: 'Type 6' },
+    { value: 'type7', label: 'Type 7' },
+    { value: 'type8', label: 'Type 8' },
   ];
-  const [open, onOpenChange] = useState(false);
+
+  const sectors = [
+    { value: 'sector1', label: 'Sector 1' },
+    { value: 'sector2', label: 'Sector 2' },
+    { value: 'sector3', label: 'Sector 3' },
+    { value: 'sector4', label: 'Sector 4' },
+    { value: 'sector5', label: 'Sector 5' },
+    { value: 'sector6', label: 'Sector 6' },
+    { value: 'sector7', label: 'Sector 7' },
+    { value: 'sector8', label: 'Sector 8' },
+  ];
 
   const resourceFormResult = {
     identifier: '',
@@ -46,68 +67,130 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
       email: '',
     },
     isPartOf: '',
-    hasPart: '',
     homepage: '',
-    type: '',
-    sector: '',
+    type: [],
+    sector: [],
   };
 
-  const handleIdentifierChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.identifier = event.currentTarget.value;
+  const validateEmailRegexp = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
   };
 
-  const handleDescrptionChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.description.nb = event.currentTarget.value;
+  const validateOrgNr = (orgNr: string) => {
+    return String(orgNr)
+      .toLowerCase()
+      .match(/^\d{9}$/);
   };
 
-  const handleTitleChangeChange = (
-    event: React.FormEvent<HTMLInputElement>,
-  ) => {
-    resourceFormResult.title.nb = event.currentTarget.value;
+  const emailIsValid = (email: string) => {
+    if (validateEmailRegexp(email)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
-  const handleOrganizationNumberChange = (
-    event: React.FormEvent<HTMLInputElement>,
-  ) => {
-    resourceFormResult.hasCompetentAuthority.organization =
-      event.currentTarget.value;
+  const stringIsEmpty = (text: string) => {
+    if (text.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
-  const handleOrgCodeChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.hasCompetentAuthority.orgCode =
-      event.currentTarget.value;
+  const organizationNumberIsValid = (orgNr: string) => {
+    if (validateOrgNr(orgNr)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
-  const handlePhoneChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.contactPoint.phone = event.currentTarget.value;
+  const handleIdentifierChange = (event: string) => {
+    setId(event);
+    setIsValidIdentifier(!stringIsEmpty(id));
   };
 
-  const handleEmailChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.contactPoint.email = event.currentTarget.value;
+  const handleTitleChange = (event: string) => {
+    setTitle(event);
+    setIsValidTitle(!stringIsEmpty(title));
   };
 
-  const handleIsPartOfChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.isPartOf = event.currentTarget.value;
+  const handleDescrptionChange = (event: string) => {
+    setDescription(event);
+    setIsValidDescription(!stringIsEmpty(description));
   };
 
-  const handleHasPartChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.hasPart = event.currentTarget.value;
+  const handleOrganizationNumberChange = (event: string) => {
+    setOrganizationNumber(event);
+    setIsValidOrganizationNumber(organizationNumberIsValid(event));
   };
 
-  const handleHomepageChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.homepage = event.currentTarget.value;
+  const handleOrgCodeChange = (event: string) => {
+    setOrgcode(event);
+    setIsValidOrgCode(!stringIsEmpty(event));
   };
 
-  const handleTypeChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.type = event.currentTarget.value;
+  const handlePhoneChange = (event: string) => {
+    setPhone(event);
+    setIsValidPhone(!stringIsEmpty(event));
   };
 
-  const handleSectorChange = (event: React.FormEvent<HTMLInputElement>) => {
-    resourceFormResult.sector = event.currentTarget.value;
+  const handleEmailChange = (event: string) => {
+    setEmail(event);
+    setIsValidEmail(emailIsValid(event));
+  };
+
+  const handleIsPartOfChange = (event: string) => {
+    setPartOf(event);
+    setIsValidPartOf(!stringIsEmpty(event));
+  };
+
+  const handleHomepageChange = (event: string) => {
+    setHomepage(event);
+    setIsValidHomepage(!stringIsEmpty(event));
   };
 
   const submit = () => {
+    Object.values(selectSectors).forEach((sector: unknown) => {
+      return resourceFormResult.sector.push(sector.value);
+    });
+    Object.values(selectTypes).forEach((type) => {
+      resourceFormResult.type.push(type.value);
+    });
+    resourceFormResult.identifier = id;
+    resourceFormResult.title.nb = title;
+    resourceFormResult.description.nb = description;
+    resourceFormResult.hasCompetentAuthority.organization = organizationNumber;
+    resourceFormResult.hasCompetentAuthority.orgCode = orgCode;
+    resourceFormResult.contactPoint.phone = phone;
+    resourceFormResult.contactPoint.email = email;
+    resourceFormResult.isPartOf = partOf;
+    resourceFormResult.homepage = homepage;
+    isAllFieldsValid();
     console.log(resourceFormResult);
+  };
+
+  const isAllFieldsValid = () => {
+    if (
+      !(
+        isValidIdentifier &&
+        isValidTitle &&
+        isValidDescription &&
+        isValidOrganizationNumber &&
+        isValidOrgCode &&
+        isValidPhone &&
+        isValidPartOf &&
+        isValidHomepage &&
+        isValidEmail
+      )
+    ) {
+      alert('Se over at alle felter er riktig utfylt');
+    }
   };
 
   const questionMark = (
@@ -131,27 +214,10 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   Brukes til å oppgi en formell identifikasjon til tjenesten.
                 </PopoverPanel>
                 <TextField
-                  onChange={(e) => handleIdentifierChange(e)}
-                ></TextField>
-              </div>
-              <div className={classes['float-child']}>
-                <label>Tittel</label>
-                <SelectSearch
-                  options={titles}
-                  value={resourceFormResult.title.nb}
-                  placeholder="Select title"
-                  search={true}
-                />
-              </div>
-            </div>
-            <div className={classes['float-container']}>
-              <div className={classes['float-child']}>
-                <label>Identifikator</label>
-                <PopoverPanel trigger={questionMark}>
-                  Brukes til å oppgi en formell identifikasjon til tjenesten.
-                </PopoverPanel>
-                <TextField
-                  onChange={(e) => handleIdentifierChange(e)}
+                  onChange={(e) =>
+                    handleIdentifierChange(e.currentTarget.value)
+                  }
+                  isValid={isValidIdentifier}
                 ></TextField>
               </div>
               <div className={classes['float-child']}>
@@ -160,7 +226,8 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   Brukes til å oppgi det offisielle navnet på tjenesten.
                 </PopoverPanel>
                 <TextField
-                  onChange={(e) => handleTitleChangeChange(e)}
+                  onChange={(e) => handleTitleChange(e.currentTarget.value)}
+                  isValid={isValidTitle}
                 ></TextField>
               </div>
             </div>
@@ -174,7 +241,10 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   satt bort til andre.{' '}
                 </PopoverPanel>
                 <TextField
-                  onChange={(e) => handleOrganizationNumberChange(e)}
+                  onChange={(e) =>
+                    handleOrganizationNumberChange(e.currentTarget.value)
+                  }
+                  isValid={isValidOrganizationNumber}
                 ></TextField>
               </div>
               <div className={classes['float-child']}>
@@ -185,7 +255,10 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   direkte av den aktuelle offentlige organisasjonen eller er
                   satt bort til andre.{' '}
                 </PopoverPanel>
-                <TextField onChange={(e) => handleOrgCodeChange(e)}></TextField>
+                <TextField
+                  onChange={(e) => handleOrgCodeChange(e.currentTarget.value)}
+                  isValid={isValidOrgCode}
+                ></TextField>
               </div>
             </div>
             <div className={classes['float-container']}>
@@ -194,14 +267,20 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                 <PopoverPanel trigger={questionMark}>
                   Brukes til å oppgi kontaktpunkt(er) for tjenesten.{' '}
                 </PopoverPanel>
-                <TextField onChange={(e) => handlePhoneChange(e)}></TextField>
+                <TextField
+                  onChange={(e) => handlePhoneChange(e.currentTarget.value)}
+                  isValid={isValidPhone}
+                ></TextField>
               </div>
               <div className={classes['float-child']}>
                 <label>Epost</label>
                 <PopoverPanel trigger={questionMark}>
                   Brukes til å oppgi kontaktpunkt(er) for tjenesten.{' '}
                 </PopoverPanel>
-                <TextField onChange={(e) => handleEmailChange(e)}></TextField>
+                <TextField
+                  onChange={(e) => handleEmailChange(e.currentTarget.value)}
+                  isValid={isValidEmail}
+                ></TextField>
               </div>
             </div>
             <div className={classes['float-container']}>
@@ -212,7 +291,10 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   egenskapen kan gjentas når beskrivelsen finnes i flere språk.{' '}
                 </PopoverPanel>
                 <TextField
-                  onChange={(e) => handleDescrptionChange(e)}
+                  onChange={(e) =>
+                    handleDescrptionChange(e.currentTarget.value)
+                  }
+                  isValid={isValidDescription}
                 ></TextField>
               </div>
               <div className={classes['float-child']}>
@@ -221,7 +303,8 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   Hjemmeside til tjenesten.
                 </PopoverPanel>
                 <TextField
-                  onChange={(e) => handleHomepageChange(e)}
+                  onChange={(e) => handleHomepageChange(e.currentTarget.value)}
+                  isValid={isValidHomepage}
                 ></TextField>
               </div>
             </div>
@@ -233,16 +316,9 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   tjenesten er en del av.
                 </PopoverPanel>
                 <TextField
-                  onChange={(e) => handleIsPartOfChange(e)}
+                  onChange={(e) => handleIsPartOfChange(e.currentTarget.value)}
+                  isValid={isValidPartOf}
                 ></TextField>
-              </div>
-              <div className={classes['float-child']}>
-                <label>Har part</label>
-                <PopoverPanel trigger={questionMark}>
-                  Brukes til å referere til en annen tjeneste som denne
-                  tjenesten er en del av.{' '}
-                </PopoverPanel>
-                <TextField onChange={(e) => handleHasPartChange(e)}></TextField>
               </div>
             </div>
             <div className={classes['float-container']}>
@@ -252,7 +328,19 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   Brukes til å indikere type tjeneste i henhold til et
                   kontrollert vokabular.
                 </PopoverPanel>
-                <TextField onChange={(e) => handleTypeChange(e)}></TextField>
+                <Select
+                  isMulti
+                  options={types}
+                  theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 0,
+                    colors: {
+                      ...theme.colors,
+                      neutral20: '#008fd6',
+                    },
+                  })}
+                  onChange={setSelectTypes}
+                />
               </div>
               <div className={classes['float-child']}>
                 <label>Sektor</label>
@@ -261,7 +349,19 @@ const ResourceRegistry: FC<ResourceRegistryProps> = () => {
                   offentlige tjenesten er relatert til, eller er ment for. En
                   tjeneste kan relateres til flere industrier/sektorer.
                 </PopoverPanel>
-                <TextField onChange={(e) => handleSectorChange(e)}></TextField>
+                <Select
+                  isMulti
+                  options={sectors}
+                  theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 0,
+                    colors: {
+                      ...theme.colors,
+                      neutral20: '#008fd6',
+                    },
+                  })}
+                  onChange={setSelectSectors}
+                />
               </div>
             </div>
             <div className={classes['resource-registry__submit-button']}>

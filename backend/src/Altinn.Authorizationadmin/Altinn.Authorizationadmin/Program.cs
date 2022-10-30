4f73dc545eebf92f2296c5f205855fa75c10dfb2
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2022-10-30 22:00:55 +0100

    Added swagger option to send in authorization token

--- backend/src/Altinn.Authorizationadmin/Altinn.Authorizationadmin/Program.cs
+++ backend/src/Altinn.Authorizationadmin/Altinn.Authorizationadmin/Program.cs
@@ -33,7 +33,9 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Npgsql.Logging;
+using Swashbuckle.AspNetCore.Filters;
 using Yuniql.AspNetCore;
 using Yuniql.PostgreSql;
 
@@ -194,7 +196,17 @@ void ConfigureServices(IServiceCollection services, IConfiguration config)
     builder.Services.AddEndpointsApiExplorer();
 
     services.AddHealthChecks().AddCheck<HealthCheck>("authorization_admin_health_check");
-    services.AddSwaggerGen();
+    services.AddSwaggerGen(options =>
+    {
+        options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+        {
+            Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
+        options.OperationFilter<SecurityRequirementsOperationFilter>();
+    });
     services.AddMvc();
 
     GeneralSettings generalSettings = config.GetSection("GeneralSettings").Get<GeneralSettings>();

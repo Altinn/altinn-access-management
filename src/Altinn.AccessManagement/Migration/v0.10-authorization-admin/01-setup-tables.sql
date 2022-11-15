8c6a32d76466dd09a81c2c7497c0cd73f6325fb9
Remi André Løvoll <remi.lovoll@avanade.com>
2022-11-15 16:14:08 +0100

    Removed DB change to let DB be refactored

--- src/Altinn.AccessManagement/Migration/v0.10-authorization-admin/01-setup-tables.sql
+++
@@ -1,6 +0,0 @@
---Adding columns to the table delegation.delegationChanges
-ALTER TABLE delegation.delegationChanges
-ADD COLUMN performedbypartyid integer,
-ALTER COLUMN performedbyuserid DROP NOT NULL;
-
-

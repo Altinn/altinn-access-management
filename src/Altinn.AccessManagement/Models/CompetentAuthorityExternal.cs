6e5e43f6ac42477ca9eb6ef01ad5280c0f9a2c15
Rune T. Larsen <rune@hjemmekino.no>
2022-12-22 14:07:08 +0100

    Added name and fixed mapper issue and launch settings

---
+++ src/Altinn.AccessManagement/Models/CompetentAuthorityExternal.cs
@@ -0,0 +1,23 @@
+﻿namespace Altinn.AccessManagement.Models
+{
+    /// <summary>
+    /// Model representation of Competent Authority part of the ServiceResource model
+    /// </summary>
+    public class CompetentAuthorityExternal
+    {
+        /// <summary>
+        /// The organization number
+        /// </summary>
+        public string Organization { get; set; }
+
+        /// <summary>
+        /// The organization code
+        /// </summary>
+        public string Orgcode { get; set; }
+
+        /// <summary>
+        /// The organization name. If not set it will be retrived from register based on Organization number
+        /// </summary>
+        public Dictionary<string, string> Name { get; set; }
+    }
+}

471252cab8521c659ae25dde83ef21f0f75571cb
Remi André Løvoll <56019927+lovoll@users.noreply.github.com>
2023-01-11 14:50:22 +0100

    Feature/0100/0198 remove token validation (#230)
    Co-authored-by: acn-dgopa <acn-dgopa@dev-acn-tje-14>
    Co-authored-by: Rune T. Larsen <rune@hjemmekino.no>
    Co-authored-by: Dhanalakshmi Gopalswamy <34273718+acn-dgopa@users.noreply.github.com>

---
+++ test/Altinn.AccessManagement.Tests/Mocks/SigningKeyResolverMock.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Altinn.Common.AccessToken.Services;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Altinn.AccessManagement.Tests.Mocks
+{
+    /// <summary>
+    /// Mock for finding signing key on disck
+    /// </summary>
+    public class SigningKeyResolverMock : ISigningKeysResolver
+    {
+        /// <summary>
+        /// Not implemented
+        /// </summary>
+        public SigningCredentials GetSigningCredentials()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task<IEnumerable<SecurityKey>> GetSigningKeys(string issuer)
+        {
+            List<SecurityKey> signingKeys = new List<SecurityKey>();
+
+            X509Certificate2 cert = new X509Certificate2($"{issuer}-org.pem");
+            SecurityKey key = new X509SecurityKey(cert);
+
+            signingKeys.Add(key);
+
+            return Task.FromResult(signingKeys.AsEnumerable());
+        }
+    }
+}

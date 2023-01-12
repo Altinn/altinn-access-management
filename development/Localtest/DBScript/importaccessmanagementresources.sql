cc3565253e861844e0140f09dd404523f450d1c0
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2023-01-12 13:12:45 +0100

    Removed column resourcestatus from improt script

--- development/Localtest/DBScript/importaccessmanagementresources.sql
+++ development/Localtest/DBScript/importaccessmanagementresources.sql
@@ -8,8 +8,8 @@ COPY temp_import_amresource from '../testdata/accessmanagementresourcend.json';
 
 select * from temp_import_amresource;
 
-INSERT INTO accessmanagement.resource (resourceregistryid, resourcetype, resourcestatus, created, modified)
-SELECT (doc->>'resourceregistryid')::text, (doc->>'resourcetype')::text, (doc->>'resourcestatus')::text, (doc->>'created')::timestamp,(doc->>'modified')::timestamp
+INSERT INTO accessmanagement.resource (resourceregistryid, resourcetype, created, modified)
+SELECT (doc->>'resourceregistryid')::text, (doc->>'resourcetype')::text, (doc->>'created')::timestamp,(doc->>'modified')::timestamp
 FROM temp_import_amresource as tmp
 WHERE not exists (select resourceregistryid from accessmanagement.resource where resourceregistryid = (tmp.doc->>'resourceregistryid')::text);
 

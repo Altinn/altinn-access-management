d9cdba0634dfdb531eee36f6146f1e2d7fa0fa53
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2023-01-24 17:03:36 +0100

    Update importaccessmanagementresources.sql

--- development/Localtest/DBScript/importaccessmanagementresources.sql
+++ development/Localtest/DBScript/importaccessmanagementresources.sql
@@ -4,8 +4,7 @@ CREATE TEMPORARY TABLE temp_import_amresource (doc JSON);
 
 select * from temp_import_amresource;
 -- copy file content to newly created table
-COPY temp_import_amresource from '/Users/albertlarsen/RiderProjects/altinn-access-management/development/Localtest/Testdata/accessmanagementresourcend.json';
-
+COPY temp_import_amresource from '../testdata/accessmanagementresourcend.json';
 select * from temp_import_amresource;
 
 INSERT INTO accessmanagement.resource (resourceregistryid, resourcetype, created, modified)

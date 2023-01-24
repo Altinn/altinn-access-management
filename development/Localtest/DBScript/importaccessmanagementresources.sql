4766e1ecb5ecffaa30f3fb36c3fa1da1fac250d6
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2023-01-24 17:04:52 +0100

    Update importaccessmanagementresources.sql

--- development/Localtest/DBScript/importaccessmanagementresources.sql
+++ development/Localtest/DBScript/importaccessmanagementresources.sql
@@ -1,11 +1,12 @@
 --drop table temp_import;
 -- create temporary table for storing json data
 CREATE TEMPORARY TABLE temp_import_amresource (doc JSON);
-
 select * from temp_import_amresource;
 -- copy file content to newly created table
 COPY temp_import_amresource from '../testdata/accessmanagementresourcend.json';
+
 select * from temp_import_amresource;
+
 INSERT INTO accessmanagement.resource (resourceregistryid, resourcetype, created, modified)
 SELECT (doc->>'resourceregistryid')::text, (doc->>'resourcetype')::text, (doc->>'created')::timestamp,(doc->>'modified')::timestamp
 FROM temp_import_amresource as tmp

3529b1112d66da2fd0c29b97d91346c2f2bf0a73
Rune Tømmerås Larsen <rune@hjemmekino.no>
2023-05-02 13:07:20 +0200

    Fixed incorrect mapping (#426)
    * Fixed incorrect mapping
    
    * Fix of testdata

--- src/Altinn.AccessManagement/Mappers/AccessManagementMapper.cs
+++ src/Altinn.AccessManagement/Mappers/AccessManagementMapper.cs
@@ -20,8 +20,8 @@ namespace Altinn.AccessManagement.Mappers
             CreateMap<Delegation, DelegationExternal>();
             CreateMap<Delegation, MaskinportenSchemaDelegationExternal>();
             CreateMap<Delegation, MPDelegationExternal>()
-                .ForMember(dest => dest.SupplierOrg, act => act.MapFrom(src => src.OfferedByOrganizationNumber))
-                .ForMember(dest => dest.ConsumerOrg, act => act.MapFrom(src => src.CoveredByOrganizationNumber))
+                .ForMember(dest => dest.SupplierOrg, act => act.MapFrom(src => src.CoveredByOrganizationNumber))
+                .ForMember(dest => dest.ConsumerOrg, act => act.MapFrom(src => src.OfferedByOrganizationNumber))
                 .ForMember(dest => dest.DelegationSchemeId, act => act.MapFrom(src => src.ResourceReferences.Find(rf => rf.ReferenceType == ReferenceType.DelegationSchemeId).Reference))
                 .ForMember(dest => dest.Scopes, act => act.MapFrom(src => src.ResourceReferences.Where(rf => string.Equals(rf.ReferenceType, ReferenceType.MaskinportenScope)).Select(rf => rf.Reference).ToList()))
                 .ForMember(dest => dest.Created, act => act.MapFrom(src => src.Created))

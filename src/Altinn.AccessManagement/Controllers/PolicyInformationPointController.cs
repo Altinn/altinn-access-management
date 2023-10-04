fdecc6743342f770ad1032af0669b1c518409e0e
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-10-04 09:07:58 +0200

    Start implementing controller

---
+++ src/Altinn.AccessManagement/Controllers/PolicyInformationPointController.cs
@@ -0,0 +1,18 @@
+// <copyright file="PolicyInformationPointController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+using Altinn.AccessManagement.Core.Services.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.AccessManagement.Controllers
+{
+    public class PolicyInformationPointController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IPolicyInformationPoint;
+        private readonly IMapper _mapper;
+
+        
+    }
+}

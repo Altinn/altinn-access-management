8ff8fc35f04f41d09fc8df749704e8f5ef463c88
Andreas Isnes <andreas.isnes@gmail.com>
2024-01-08 13:16:26 +0100

    remove async warning for DelegationRequestsController

--- src/Altinn.AccessManagement/Controllers/DelegationRequestsController.cs
+++ src/Altinn.AccessManagement/Controllers/DelegationRequestsController.cs
@@ -38,7 +38,7 @@ namespace Altinn.AccessManagement.Controllers
         [ApiExplorerSettings(IgnoreApi = true)]
         public async Task<DelegationRequests> Get(string who, [FromQuery] string? serviceCode = "", [FromQuery] int? serviceEditionCode = null, [FromQuery] RestAuthorizationRequestDirection direction = RestAuthorizationRequestDirection.Both, [FromQuery] List<RestAuthorizationRequestStatus>? status = null, [FromQuery] string? continuation = "")
         {
-           return await _delegationRequests.GetDelegationRequestsAsync(who, serviceCode, serviceEditionCode, direction, status, continuation);
+            return await _delegationRequests.GetDelegationRequestsAsync(who, serviceCode, serviceEditionCode, direction, status, continuation);
         }
 
         /// <summary>
@@ -49,12 +49,17 @@ namespace Altinn.AccessManagement.Controllers
         [HttpGet("accessmanagement/api/v1/delegationrequests/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ApiExplorerSettings(IgnoreApi = true)]
-        public async Task<ActionResult<DelegationRequest>> Get(string id)
+        public Task<ActionResult<DelegationRequest>> Get(string id)
         {
-            DelegationRequest delegationRequest = new DelegationRequest();
-            delegationRequest.RequestResources = new List<AuthorizationRequestResource>();
-            delegationRequest.RequestResources.Add(new AuthorizationRequestResource() { ServiceCode = "asdf", ServiceEditionCode = 435 });
-            return delegationRequest;
+            DelegationRequest delegationRequest = new()
+            {
+                RequestResources =
+                [
+                    new AuthorizationRequestResource() { ServiceCode = "asdf", ServiceEditionCode = 435 },
+                ]
+            };
+
+            return Task.FromResult<ActionResult<DelegationRequest>>(delegationRequest);
         }
     }
 }

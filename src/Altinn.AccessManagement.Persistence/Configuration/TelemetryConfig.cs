a02ab84b5c4c2fa28def432ef2511a097c9d60b8
Marius Thuen <thuen@spirh.no>
2024-03-06 09:38:11 +0100

    Added OpenTelemetry

---
+++ src/Altinn.AccessManagement.Persistence/Configuration/TelemetryConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.AccessManagement.Persistence.Configuration
+{
+    public static class TelemetryConfig
+    {
+        public static readonly ActivitySource activitySource = new ActivitySource("Altinn.AccessManagement.Persistence");
+    }
+}

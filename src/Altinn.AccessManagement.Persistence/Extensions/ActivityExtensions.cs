f929551c9ac4afffcb8474d759756cd67a5fb964
Marius Thuen <thuen@spirh.no>
2024-03-08 16:13:05 +0100

    Hotfix for bug with AddSqlTags in Persistence

--- src/Altinn.AccessManagement.Persistence/Extensions/ActivityExtensions.cs
+++ src/Altinn.AccessManagement.Persistence/Extensions/ActivityExtensions.cs
@@ -19,31 +19,30 @@ public static class ActivityExtensions
     /// <param name="command">NpgsqlCommand</param>
     public static void AddSqlTags(this Activity activity, NpgsqlCommand command)
     {
-        activity.SetTag("db.system", "Postgres");
-        activity.SetTag("db.statement", command.CommandText);
-
-        foreach (var param in command.Parameters.ToList())
+        try
         {
-            try
+            activity.SetTag("db.system", "Postgres");
+            activity.SetTag("db.statement", command.CommandText);
+            foreach (var param in command.Parameters.ToList())
             {
-                if (param.Value == null) 
-                { 
-                    continue; 
-                }
+                    if (param.Value == null) 
+                    { 
+                        continue; 
+                    }
 
-                if (param.Value is IList)
-                {
-                    activity.SetTag(param.ParameterName, string.Join(',', (IList)param.Value));
-                }
-                else
-                {
-                    activity.SetTag(param.ParameterName, param.Value);
-                }
+                    if (param.Value is IList)
+                    {
+                        activity.SetTag(param.ParameterName, string.Join(',', (IList)param.Value));
+                    }
+                    else
+                    {
+                        activity.SetTag(param.ParameterName, param.Value);
+                    }
             }
-            catch (Exception ex) 
-            { 
-                activity.RecordException(ex);
-            }
+        }
+        catch (Exception ex) 
+        { 
+            activity.RecordException(ex);
         }
     }
 

a5392002ed89cc69cccd8e86de8b72c175544993
Marius Thuen <thuen@spirh.no>
2024-03-08 16:37:50 +0100

    Hotfix for bug with AddSqlTags in Persistence (#639)
    * Hotfix for bug with AddSqlTags in Persistence
    
    * Removed AddSqlTags from ActivityExtensions

--- src/Altinn.AccessManagement.Persistence/Extensions/ActivityExtensions.cs
+++ src/Altinn.AccessManagement.Persistence/Extensions/ActivityExtensions.cs
@@ -13,50 +13,15 @@ namespace Altinn.AccessManagement.Persistence.Extensions;
 public static class ActivityExtensions
 {
     /// <summary>
-    /// Sets Activity tags based on command
-    /// </summary>
-    /// <param name="activity">Current activity</param>
-    /// <param name="command">NpgsqlCommand</param>
-    public static void AddSqlTags(this Activity activity, NpgsqlCommand command)
-    {
-        activity.SetTag("db.system", "Postgres");
-        activity.SetTag("db.statement", command.CommandText);
-
-        foreach (var param in command.Parameters.ToList())
-        {
-            try
-            {
-                if (param.Value == null) 
-                { 
-                    continue; 
-                }
-
-                if (param.Value is IList)
-                {
-                    activity.SetTag(param.ParameterName, string.Join(',', (IList)param.Value));
-                }
-                else
-                {
-                    activity.SetTag(param.ParameterName, param.Value);
-                }
-            }
-            catch (Exception ex) 
-            { 
-                activity.RecordException(ex);
-            }
-        }
-    }
-
-    /// <summary>
     /// Sets status and records exception
     /// </summary>
     /// <param name="activity">Current activity</param>
     /// <param name="ex">Exception to record</param>
     /// <param name="statusDescription">Optional description/message for error</param>
-    public static void ErrorWithException(this Activity activity, Exception ex, string? statusDescription = null)
+    public static void ErrorWithException(this Activity? activity, Exception ex, string? statusDescription = null)
     {
-        activity.RecordException(ex);
-        activity.SetStatus(ActivityStatusCode.Error, statusDescription);
+        activity?.RecordException(ex);
+        activity?.SetStatus(ActivityStatusCode.Error, statusDescription);
     }
 
     /// <summary>
@@ -65,13 +30,13 @@ public static class ActivityExtensions
     /// <param name="activity">Current activity</param>
     /// <param name="statusDescription">Optional description/message for error</param>
     /// <param name="resultSize">Optional metric of resultsize</param>
-    public static void FinishedOk(this Activity activity, string? statusDescription = null, int? resultSize = null)
+    public static void FinishedOk(this Activity? activity, string? statusDescription = null, int? resultSize = null)
     {
         if (resultSize != null) 
         {
-            activity.SetTag("ResultSize", resultSize.Value);
+            activity?.SetTag("ResultSize", resultSize.Value);
         }
 
-        activity.SetStatus(ActivityStatusCode.Ok, statusDescription);
+        activity?.SetStatus(ActivityStatusCode.Ok, statusDescription);
     }
 }

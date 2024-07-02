d8376c704299c0caf05e06fb1741cda87129aef9
Marius Thuen <thuen@spirh.no>
2024-07-02 12:51:25 +0200

    #558 - Sql querystring bug (#750)
    * #558 - Sql querystring bug
    
    * #558 - Removed DbNull values

--- src/Altinn.AccessManagement.Persistence/DelegationMetadataRepo.cs
+++ src/Altinn.AccessManagement.Persistence/DelegationMetadataRepo.cs
@@ -209,9 +209,9 @@ namespace Altinn.AccessManagement.Persistence
                 { "delegationChangeType", delegationChange.DelegationChangeType },
                 { "altinnAppId", delegationChange.ResourceId },
                 { "offeredByPartyId", delegationChange.OfferedByPartyId },
-                { "coveredByUserId", delegationChange.CoveredByUserId.HasValue ? delegationChange.CoveredByUserId.Value : DBNull.Value },
-                { "coveredByPartyId", delegationChange.CoveredByPartyId.HasValue ? delegationChange.CoveredByPartyId.Value : DBNull.Value },
-                { "performedByUserId", delegationChange.PerformedByUserId.HasValue ? delegationChange.PerformedByUserId.Value : DBNull.Value },
+                { "coveredByUserId", delegationChange.CoveredByUserId },
+                { "coveredByPartyId", delegationChange.CoveredByPartyId },
+                { "performedByUserId", delegationChange.PerformedByUserId },
                 { "blobStoragePolicyPath", delegationChange.BlobStoragePolicyPath },
                 { "blobStorageVersionId", delegationChange.BlobStorageVersionId }
             };
@@ -244,10 +244,10 @@ namespace Altinn.AccessManagement.Persistence
                 { "delegationChangeType", delegationChange.DelegationChangeType },
                 { "altinnAppId", delegationChange.ResourceId },
                 { "offeredByPartyId", delegationChange.OfferedByPartyId },
-                { "coveredByUserId", delegationChange.CoveredByUserId.HasValue ? delegationChange.CoveredByUserId.Value : DBNull.Value },
-                { "coveredByPartyId", delegationChange.CoveredByPartyId.HasValue ? delegationChange.CoveredByPartyId.Value : DBNull.Value },
-                { "performedByUserId", delegationChange.PerformedByUserId.HasValue ? delegationChange.PerformedByUserId.Value : DBNull.Value },
-                { "performedByPartyId", delegationChange.PerformedByPartyId.HasValue ? delegationChange.PerformedByUserId.Value : DBNull.Value },
+                { "coveredByUserId", delegationChange.CoveredByUserId },
+                { "coveredByPartyId", delegationChange.CoveredByPartyId },
+                { "performedByUserId", delegationChange.PerformedByUserId },
+                { "performedByPartyId", delegationChange.PerformedByPartyId },
                 { "blobStoragePolicyPath", delegationChange.BlobStoragePolicyPath },
                 { "blobStorageVersionId", delegationChange.BlobStorageVersionId },
                 { "delegatedTime", delegationChange.Created.HasValue ? delegationChange.Created.Value : DateTime.UtcNow }
@@ -361,22 +361,22 @@ namespace Altinn.AccessManagement.Persistence
             SELECT MAX(DC.resourceRegistryDelegationChangeId) AS changeId, R.resourceId, R.resourceRegistryId, R.resourceType
             FROM accessmanagement.Resource AS R
             INNER JOIN delegation.ResourceRegistryDelegationChanges AS DC ON R.resourceid = DC.resourceid_fk
-            WHERE DC.offeredByPartyId = @offeredByPartyId
+            WHERE DC.offeredByPartyId = @offeredByPartyId 
             """;
 
             if (resourceRegistryIds != null && resourceRegistryIds.Count > 0)
             {
-                query += "AND resourceRegistryId = ANY (@resourceRegistryIds)";
+                query += " AND resourceRegistryId = ANY (@resourceRegistryIds) ";
                 param.Add("resourceRegistryIds", resourceRegistryIds);
             }
 
             if (resourceTypes != null && resourceTypes.Count > 0)
             {
-                query += "AND resourceType = ANY (@resourceTypes)";
+                query += " AND resourceType = ANY (@resourceTypes) ";
                 param.Add("resourceTypes", resourceTypes.Select(t => t.ToString().ToLower()).ToList());
             }
 
-            query += "GROUP BY resourceId_fk, offeredByPartyId, coveredByPartyId, coveredByUserId, R.resourceId, R.resourceRegistryId, R.resourceType)";
+            query += " GROUP BY resourceId_fk, offeredByPartyId, coveredByPartyId, coveredByUserId, R.resourceId, R.resourceRegistryId, R.resourceType) ";
 
             query +=
             /*strpsql*/"""

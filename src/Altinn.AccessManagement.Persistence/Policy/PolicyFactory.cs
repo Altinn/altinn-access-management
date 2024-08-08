5960b9cc60068ec6b926ddb2cc47ca9ca918004a
Andreas Isnes <andreas.isnes@gmail.com>
2024-08-08 10:38:23 +0200

    add new policy factory

---
+++ src/Altinn.AccessManagement.Persistence/Policy/PolicyFactory.cs
@@ -0,0 +1,45 @@
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Options;
+
+namespace Altinn.AccessManagement.Persistence.Policy;
+
+/// <inheritdoc/>
+public class PolicyFactory(IAzureClientFactory<BlobServiceClient> factory, IOptionsFactory<PolicyOptions> options) : IPolicyFactory
+{
+    /// <summary>
+    /// Factory for creating clients
+    /// </summary>
+    /// <value>asd</value>
+    public IAzureClientFactory<BlobServiceClient> Factory { get; } = factory;
+
+    /// <summary>
+    /// Factory for getting configuration for client factory
+    /// </summary>
+    public IOptionsFactory<PolicyOptions> Options { get; } = options;
+
+    /// <inheritdoc/>
+    public IPolicyRepositoryV2 Create(AccountType account, string blob)
+    {
+        var client = Factory
+            .CreateClient(account.ToString())
+            .CreateBlobContainer(Options.Create(account.ToString()).Container).Value
+            .GetBlobClient(blob);
+
+        return new PolicyRepositoryV2(client);
+    }
+}
+
+/// <summary>
+/// Create clients for interacting with files 
+/// </summary>
+public interface IPolicyFactory
+{
+    /// <summary>
+    /// Creates a client for interacting with storage
+    /// </summary>
+    /// <param name="account">which storage account to write blob</param>
+    /// <param name="filepath">path of the file</param>
+    /// <returns></returns>
+    IPolicyRepositoryV2 Create(AccountType account, string filepath);
+}

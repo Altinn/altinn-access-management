4395a55fd42212a3f7c11b95aa7623bff4cce143
Andreas Isnes <andreas.isnes@gmail.com>
2024-08-12 15:41:55 +0200

    add temp logger

--- src/Altinn.AccessManagement/Program.cs
+++ src/Altinn.AccessManagement/Program.cs
@@ -80,8 +80,8 @@ void ConfigureSetupLogging()
         builder
             .AddFilter("Microsoft", LogLevel.Warning)
             .AddFilter("System", LogLevel.Warning)
-            .AddFilter("Altinn.AccessManagement.Program", LogLevel.Debug);
-        // .AddConsole();
+            .AddFilter("Altinn.AccessManagement.Program", LogLevel.Debug)
+            .AddConsole();
     });
 
     logger = logFactory.CreateLogger<Program>();

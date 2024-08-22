bb2dc87b9734fd00333f05229adc3cff2c3e871f
Ivar Olav Tryti <aas-iotryti@ai-dev.no>
2024-08-22 13:46:57 +0200

    tokengenerator now takes more variables

--- test/Bruno/Altinn.AccessManagement/TestToolsTokenGenerator.js
+++ test/Bruno/Altinn.AccessManagement/TestToolsTokenGenerator.js
@@ -1,5 +1,5 @@
 
-exports.getToken = async function (scopes) {
+exports.getToken = async function (scopes, tokenPid, tokenUser, tokenParty) {
   const axios = require("axios");
   const btoa = require("btoa");
 
@@ -10,25 +10,30 @@ exports.getToken = async function (scopes) {
 
   const tokenEnv = bru.getEnvVar("tokenEnv");
   const tokenType = bru.getVar("auth_tokenType");
-  const tokenUser = bru.getVar("auth_userId");
-  const tokenParty = bru.getVar("auth_partyId");
-  const tokenPid = bru.getVar("auth_ssn");
-  const tokenScopes = scopes;
+  // const tokenUser = auth_userId;
+  // const tokenParty = bru.getVar("auth_partyId");
+  // const tokenPid = bru.getVar("auth_ssn");
+  // const tokenScopes = scopes;
   const tokenOrg = bru.getVar("auth_org");
   const tokenOrgNo = bru.getVar("auth_orgNo");
   const tokenUsername = bru.getVar("auth_username");
 
+  console.log("tokenGenerator scopes: " + scopes);
+  console.log("tokenGenerator tokenPid: " + tokenPid);
+  console.log("tokenGenerator tokenUser: " + tokenUser);
+  console.log("tokenGenerator tokenParty: " + tokenParty);
+  
   let tokenUrl;
   if (tokenType == "Personal") {
-    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&pid=${tokenPid}&userid=${tokenUser}&partyid=${tokenParty}&authLvl=3&ttl=3000`;
+    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${scopes}&pid=${tokenPid}&userid=${tokenUser}&partyid=${tokenParty}&authLvl=3&ttl=3000`;
   }
 
   if (tokenType == "Enterprise") {
-    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&org=${tokenOrg}&orgNo=${tokenOrgNo}&ttl=30`;
+    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${scopes}&org=${tokenOrg}&orgNo=${tokenOrgNo}&ttl=30`;
   }
 
   if (tokenType == "EnterpriseUser") {
-    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&orgNo=${tokenOrgNo}&userId=${tokenUser}&partyId=${tokenParty}&userName=${tokenUserName}&ttl=30`;
+    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${scopes}&orgNo=${tokenOrgNo}&userId=${tokenUser}&partyId=${tokenParty}&userName=${tokenUserName}&ttl=30`;
   }
 
   // console.log("tokenUrl:" +  tokenUrl);

dec905b085563c0a625a6a23a274f6a3907fa945
ivartryti <59682632+ivartryti@users.noreply.github.com>
2024-09-11 12:19:28 +0200

    fix bruno tests so they can be run on pipelines (#809)
    * minor fixes so tests run on all environments
    
    * tokengenerator now takes more variables
    
    * added tokentype to testtokengenerator
    
    * edited a few tests to work in pipeline
    
    * fixed rightsenternal_altinn2 tests
    
    * fixed all remaining tests except one
    Lots of comments still need cleanup
    
    * fixed remaining tests
    still needs cleanup
    
    * fjernet console.log linjer
    la til litt debug console.log linjer
    
    * fjernet supplierorg og consumerorg
    
    * henter "scope" fra sharedtestdata-filen nå
    
    * removed supplierorg and consumerorg from url
    
    * fjernet kommentarer
    
    * removed all console.log lines
    
    ---------
    
    Co-authored-by: Ivar Olav Tryti <aas-iotryti@ai-dev.no>

--- test/Bruno/Altinn.AccessManagement/TestToolsTokenGenerator.js
+++ test/Bruno/Altinn.AccessManagement/TestToolsTokenGenerator.js
@@ -1,5 +1,5 @@
 
-exports.getToken = async function (scopes) {
+exports.getToken = async function (getTokenParameters) {
   const axios = require("axios");
   const btoa = require("btoa");
 
@@ -9,29 +9,34 @@ exports.getToken = async function (scopes) {
   const Authorization = 'Basic ' + btoa(`${basicAuthUser}:${basicAuthPw}`);
 
   const tokenEnv = bru.getEnvVar("tokenEnv");
-  const tokenType = bru.getVar("auth_tokenType");
-  const tokenUser = bru.getVar("auth_userId");
-  const tokenParty = bru.getVar("auth_partyId");
-  const tokenPid = bru.getVar("auth_ssn");
-  const tokenScopes = scopes;
-  const tokenOrg = bru.getVar("auth_org");
-  const tokenOrgNo = bru.getVar("auth_orgNo");
-  const tokenUsername = bru.getVar("auth_username");
+  const tokenType = getTokenParameters.auth_tokenType;
+  const tokenScopes = getTokenParameters.auth_scopes;
 
   let tokenUrl;
+
   if (tokenType == "Personal") {
+    const tokenUser = getTokenParameters.auth_userId;
+    const tokenParty = getTokenParameters.auth_partyId;
+    const tokenPid = getTokenParameters.auth_ssn;
+
     tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&pid=${tokenPid}&userid=${tokenUser}&partyid=${tokenParty}&authLvl=3&ttl=3000`;
   }
 
-  if (tokenType == "Enterprise") {
+  else if (tokenType == "Enterprise") {
+    const tokenOrg = getTokenParameters.auth_org;
+    const tokenOrgNo = getTokenParameters.auth_orgNo;
+
     tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&org=${tokenOrg}&orgNo=${tokenOrgNo}&ttl=30`;
   }
 
-  if (tokenType == "EnterpriseUser") {
+  else if (tokenType == "EnterpriseUser") {
+    const tokenUser = getTokenParameters.auth_userId;
+    const tokenParty = getTokenParameters.auth_partyId;
+    const tokenOrgNo = getTokenParameters.auth_orgNo;
+    const tokenUserName = getTokenParameters.auth_username;
+
     tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&orgNo=${tokenOrgNo}&userId=${tokenUser}&partyId=${tokenParty}&userName=${tokenUserName}&ttl=30`;
   }
-
-  // console.log("tokenUrl:" +  tokenUrl);
 
   const response = await axios.get(tokenUrl, {
     headers: { Authorization }

6a1cc810038a989620cf7d2b9064384d5e6e78a4
Remi Løvoll <remi.lovoll@avanade.com>
2024-10-29 14:13:40 +0100

    Unsaved changes missed from merge

--- src/Altinn.AccessManagement.Core/Helpers/DelegationHelper.cs
+++ src/Altinn.AccessManagement.Core/Helpers/DelegationHelper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using Altinn.AccessManagement.Core.Constants;
 using Altinn.AccessManagement.Core.Enums;
 using Altinn.AccessManagement.Core.Helpers.Extensions;
